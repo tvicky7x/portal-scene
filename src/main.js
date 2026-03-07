697e4b7f925408e37223352b2a8ba15d206857fd
TvickyPace <tvicky.pacefin@gmail.com>
2026-03-08 01:54:00 +0530

    feat: tried file changed

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import Stats from "stats.js";
 import GUI from "lil-gui";
-import portalVertexShader from "./shaders/portal/vertex.glsl";
-import portalFragmentShader from "./shaders/portal/fragment.glsl";
-import firefliesVertexShader from "./shaders/fireflies/vertex.glsl";
-import firefliesFragmentShader from "./shaders/fireflies/fragment.glsl";
+import portalVertexShader from "shaders/portal/vertex.glsl";
+import portalFragmentShader from "shaders/portal/fragment.glsl";
+import firefliesVertexShader from "shaders/fireflies/vertex.glsl";
+import firefliesFragmentShader from "shaders/fireflies/fragment.glsl";
 
 const stats = new Stats();
 stats.showPanel(0);
